c756e110eb393733df818d9b1cddfc8be5085d9e
Matthew Patterson <matthew.patterson@maine.edu>
2024-01-22 16:51:25 -0500

    I am currently able to tokenize and evaluate expressions.
    I need to finish implementing variables.
    
    Variable names are stored in a Dictionary, need to dynamically either assign value after evaluating tokens, or find the variable key in the Dictionary and convert it to its value as a token for expressions.

--- main.py
+++ main.py
@@ -1,16 +1,98 @@
-# This is a sample Python script.
+# Title: Homework 01: PMPVI Program
+# By: Matthew Patterson
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Variables
+literals = ["+", "-", "(", ")", "=", " "]
+userInput = ""
+userTokens = []
+userVariableTokens = {}
 
+# Functions
+def TokenizeInput(input):
+    curIndex = 0
+    curToken = ""
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # Iterate through input
+    for i in input:
+        if i not in literals:
+            # 'i' is not a literal, thus is part of curToken
+            curToken = curToken + i
+            if curIndex == len(input) - 1:
+                SubmitCurrentToken(curToken)
+                curToken = ""
+        elif i != " ":
+            SubmitCurrentToken(curToken)
+            curToken = ""
+            userTokens.append(i)
+        curIndex = curIndex + 1
 
+def SubmitCurrentToken(curToken):
+    if curToken.isnumeric():
+        userTokens.append(int(curToken))
+    elif curToken.isalpha():
+        if curToken in userVariableTokens:
+            userTokens.append(userVariableTokens.get(curToken))
+        else:
+            userVariableTokens[curToken] = 0
+    else:
+        print("ERROR: Did not submit token")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def EvaluateTokens(arrayOfTokens):
+    operand1 = ""
+    operator = ""
+    operand2 = ""
+    curTotal = 0;
+    isSettingToVariable = False
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while len(arrayOfTokens) > 0:
+        print(str(arrayOfTokens))
+        i = arrayOfTokens.pop(0)
+        if i == "=":
+            isSettingToVariable = True
+        elif i == "(":
+            if operand1 == "":
+                operand1 = EvaluateTokens(arrayOfTokens)
+            elif operand2 == "":
+                operand2 = EvaluateTokens(arrayOfTokens)
+                curTotal = curTotal + EvaluateCurExpression(operand1, operator, operand2)
+        elif i == ")":
+            return curTotal
+        elif i in literals:
+            operator = i
+            print("curOperator: " + str(operator))
+        elif operand1 == "":
+            operand1 = i
+            print("operand1: " + str(operand1))
+        elif operand2 == "":
+            operand2 = i
+            print("operand2: " + str(operand2))
+            curTotal = curTotal + EvaluateCurExpression(operand1, operator, operand2)
+
+        print("curTotal: " + str(curTotal))
+    if isSettingToVariable:
+        userVariableTokens[len(userVariableTokens) -1 ] = curTotal
+        return ""
+    else:
+        return curTotal
+
+def EvaluateCurExpression(operand1, operator, operand2):
+    print("Evaluating: " + str(operand1) + str(operator) + str(operand2))
+    match operator:
+        case "+":
+            return operand1 + operand2
+        case "-":
+            return operand1 - operand2
+
+while userInput != "quit":
+    userInput = input()
+    TokenizeInput(userInput)
+    if userTokens.count("(") is userTokens.count(")"):
+        print(EvaluateTokens(userTokens))
+    else:
+        print("ERROR: Missing a open/closing parentheses.")
+
+    print("User Int Tokens: " + str(userTokens))
+    print("User Var Tokens: " + str(userVariableTokens))
+
+    # Clear Tokens for loop
+    userTokens.clear()
